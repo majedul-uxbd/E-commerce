1e3e302e7626008be56a67a3329d291451a50498
MirazZim <miraz.zim.38@gmail.com>
2025-08-20 11:13:58 +0600

    Prodcut Api : Implemented get all products Function and Api. where admin can only get the list. Tested Manually on Postman Also

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const jwtAuth = require("../middleware/jwtAuth");
 const { addProduct } = require("../main/products/add-product");
+const { getAllProducts } = require("../main/products/get-all-products");
 
 const productRouter = express.Router();
 
@@ -15,6 +16,27 @@ productRouter.post("/create", jwtAuth, async (req, res) => {
             return res.status(200).send({
                 status: data.status,
                 message: data.message,
+            });
+        })
+        .catch((error) => {
+            return res.status(400).send({
+                status: error.status,
+                message: error.message,
+            });
+        });
+});
+
+productRouter.get("/", jwtAuth, async (req, res) => {
+    const requestData = {
+        user: req.user
+    };
+
+    getAllProducts(requestData)
+        .then((data) => {
+            return res.status(200).send({
+                status: data.status,
+                message: data.message,
+                products: data.products,
             });
         })
         .catch((error) => {
